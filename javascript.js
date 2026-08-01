873b913041c9cc0bf311b6ad0099bf4759ba4ebd
Brad Chapin <bradschapin@gmail.com>
2026-08-01 17:00:32 -0400

    Add function to play a round, which calls each of the previous functions and outputs an updated score

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let computerScore = 0;
+let humanScore = 0;
+let score = "Computer: " +computerScore + "," + "User: " + humanScore;
+
 const getComputerChoice = function () {
    let rnd = Math.floor(Math.random() * 3) +1;
    if (rnd === 1){
@@ -14,10 +18,42 @@ const getComputerChoice = function () {
 const getHumanChoice = function () {
     let humanChoice = prompt("please select paper, scissors, or rock");
     humanChoice = humanChoice.toLowerCase();
-    console.log(humanChoice);
     return humanChoice;
-}
+};
+
+const playRound = function (fcomputer, fhuman) {
+    let human = getHumanChoice();
+    let computer = getComputerChoice();
+    if (human === computer) {
+        console.log(score);
+        return "It's a tie!"
+    }
+    else if (human === "rock" && computer === "scissors") {
+        ++humanScore;
+        score = "Computer: " +computerScore + "," + "User: " + humanScore;
+        console.log(score);
+        return "Rock beats scissors! You win!";
+    }
+    else if (human === "paper" && computer === "rock") {
+        ++humanScore;
+        score = "Computer: " +computerScore + "," + "User: " + humanScore;
+        console.log(score);
+        return "Paper beats rock! You win!";
+    }
+    else if (human === "scissors" && computer === "paper") {
+        ++humanScore;
+        score = "Computer: " +computerScore + "," + "User: " + humanScore;
+        console.log(score);
+        return"Scissors beats paper! You win!";
+    }
+    else {
+        ++computerScore;
+        score = "Computer: " +computerScore + "," + "User: " + humanScore;
+        console.log(score);
+        console.log("Computer wins!");
+    }
+};
 
 
 
-getHumanChoice();
+console.log(playRound(), score);
