f9403866289d80710e999084a99664a57876649f
Brad Chapin <bradschapin@gmail.com>
2026-08-01 16:03:37 -0400

    Add for/else loop to assign random number to paper, scisors, and rock

--- javascript.js
+++ javascript.js
@@ -1,6 +1,14 @@
 const getComputerChoice = function () {
-   let rnd = Math.floor(Math.random() * 3) +1
-   console.log(rnd);
+   let rnd = Math.floor(Math.random() * 3) +1;
+   if (rnd === 1){
+    return "paper";
+   }
+   else if (rnd === 2) {
+    return "scissors";
+   }
+   else {
+    return "rock";
+   }
 };
 
-getComputerChoice();
+console.log(getComputerChoice());
